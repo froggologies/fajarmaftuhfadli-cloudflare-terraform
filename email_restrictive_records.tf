cf219c3d122b059dc31be08256d98c759ab935ab
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2024-02-27 14:01:08 +0700

    chore(email_restrictive_records.tf): comment out SPF record creation to disable it temporarily
    The SPF record creation has been commented out to disable it temporarily. This change was made to address an issue with the SPF record configuration.

--- email_restrictive_records.tf
+++ email_restrictive_records.tf
@@ -1,9 +1,9 @@
-resource "cloudflare_record" "SPF" {
-  zone_id = var.cloudflare_zone_id
-  type    = "TXT"
-  name    = var.main_domain
-  value   = "v=spf1 -all"
-}
+# resource "cloudflare_record" "SPF" {
+#   zone_id = var.cloudflare_zone_id
+#   type    = "TXT"
+#   name    = var.main_domain
+#   value   = "v=spf1 -all"
+# }
 
 resource "cloudflare_record" "DKIM" {
   zone_id = var.cloudflare_zone_id
