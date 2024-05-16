aae1f68466a597ce4790e4889a5f1119cbd92d55
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2024-05-16 11:45:35 +0700

    feat(tfvars): add example .tfvars file with placeholders for Cloudflare and email settings feat(gitignore): include .example_terraform.tfvars in gitignore to prevent sensitive data leakage feat(terraform): add backend configuration for Google Cloud Storage in backend.tf refactor(email_routing): comment out unused cloudflare_record resource and add additional email routing configuration for the_arcade16052024

---
+++ .example_terraform.tfvars
@@ -0,0 +1,5 @@
+cloudflare_zone_id    = ""
+cloudflare_account_id = ""
+cloudflare_api_token  = ""
+main_email            = ""
+main_domain           = ""
--- email_routing.tf
+++ email_routing.tf
@@ -9,18 +9,19 @@ resource "cloudflare_email_routing_address" "main_email" {
 }
 
 # The TXT record is configured to allow your domain to send incoming emails out to your preferred email provider.
-resource "cloudflare_record" "enable_email_routing" {
-  zone_id = var.cloudflare_zone_id
-  type    = "TXT"
-  name    = var.main_domain
-  value   = "v=spf1 include:_spf.mx.cloudflare.net ~all"
-}
+# resource "cloudflare_record" "enable_email_routing" {
+#   zone_id = var.cloudflare_zone_id
+#   type    = "TXT"
+#   name    = var.main_domain
+#   value   = "v=spf1 include:_spf.mx.cloudflare.net ~all"
+# }
 
 locals {
   emails = [
     "email",
     "cloudflare_report",
-    "twitter2"
+    "twitter2",
+    "the_arcade16052024"
   ]
 }
 
